0b1e523559c4446f35cdef7e42172bbc731f9b8b
kriskros84 <62452792+kriskros84@users.noreply.github.com>
2020-03-21 02:31:52 +0000

    Update __init__.py

---
+++ chitubox/__init__.py
@@ -0,0 +1,18 @@
+usage: chitucli [-h] --ip IP
+                [--list | --query | --axis-z | --version | --print | --upload | --download | --delete]
+                [files [files ...]]
+
+positional arguments:
+  files
+
+optional arguments:
+  -h, --help      show this help message and exit
+  --ip IP, -i IP
+  --list, -L
+  --query, -Q
+  --axis-z, -Z
+  --version, -V
+  --print, -P
+  --upload, -U
+  --download, -D
+  --delete, -X
